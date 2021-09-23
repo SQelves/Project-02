e6845ebd80edd1e6abc4dfd731185d05fbd00d9d
hrdowell <hannahrdowell@gmail.com>
2021-09-23 18:41:36 -0500

    init modal added

--- public/js/materialize/init.js
+++ public/js/materialize/init.js
@@ -4,6 +4,7 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('select').formSelect();
+    $('.modal').modal();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
