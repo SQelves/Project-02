7b0bb653e4824d6a184ed89821529de06fcbff1e
micahwaweru <micah.waweru@gmail.com>
2021-09-24 23:26:47 -0500

    fixed wishlist not displaying on profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,32 +1,39 @@
 console.log('proflie script attached')
 var dynamicName = document.getElementById('dynamicName').innerHTML;
 var giftsUl = document.getElementById('giftsUl');
+console.log(giftsUl);
 console.log(dynamicName);
 
 
 const displayGifts = function(){
   fetch('/api/gifts/displayGifts',{
-    "method": "GET",
-    "headers": {
+    'method': 'GET',
+    'headers':{
       'Content-Type': 'application/json'
     }
   })
   .then(
     function(response){
       response.json().then(function(data){
-        console.log(data);
+
         for(i=0;i<data.length;i++){
           var giftFN = data[i].firstName;
           var giftLN = data[i].lastName;
           var giftFullName = `${giftFN} ${giftLN}`;
           var giftNm = data[i].giftName;
           var appendItem = document.createElement('li');
+          console.log('Gifts' + giftsUl.innerHTML);
+       
+          
           if(giftFullName===dynamicName){
-            appendItem.innerHTML = giftNm
+          console.log('Gift name is equal to dynamic')
+              appendItem.innerHTML = giftNm
             appendItem.setAttribute('class', 'collection-item')
             giftsUl.appendChild(appendItem);
+        
           }
         }
+
       });
     }
   )
@@ -36,3 +43,4 @@ const displayGifts = function(){
 }
 
 displayGifts();
+
